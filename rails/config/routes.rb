107bf2a0bef55b54450a1c4a799dd2e4d51be0b3
ほと <mail@hoto.me>
2014-10-14 23:27:18 +0900

    OAuth設定

--- rails/config/routes.rb
+++ rails/config/routes.rb
@@ -1,10 +1,18 @@
 Rails.application.routes.draw do
-  devise_for :users
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  # root 'welcome#index'
+  root 'user#index'
+
+  devise_for :users,
+    controllers: { omniauth_callbacks: 'omniauth_callbacks' },
+    skip: [ :sessions, :registrations, :password ]
+
+  devise_scope :user do
+    get 'login',  to: 'user#login',              as: :new_user_session
+    get 'logout', to: 'devise/sessions#destroy', as: :destroy_user_session
+  end
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
