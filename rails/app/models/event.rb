b15b7f3f49b21bef0c51291f3d83635445673ee2
石川 雄基 / ほと <yuuki.ishikawa@gaiax.com>
2014-10-16 23:58:54 +0900

    イベント毎のユーザ/グループ情報を表示できるようにする

--- rails/app/models/event.rb
+++ rails/app/models/event.rb
@@ -1,4 +1,17 @@
 class Event < ActiveRecord::Base
   has_many :user_groups
   has_many :groups
+
+  def participant
+    user_groups.where is_participant: 1
+  end
+
+  def nonparticipant
+    user_groups.where is_participant: 0
+  end
+
+  def unassigned
+    participant.where group_id: nil
+  end
+
 end
