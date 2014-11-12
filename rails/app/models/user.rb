6d529f0dc67ad121f2c6039fca9055247ff648c6
石川 雄基 / ほと <yuuki.ishikawa@gaiax.com>
2014-11-12 12:59:39 +0900

    MOBじゃなくてMOBA

--- rails/app/models/user.rb
+++ rails/app/models/user.rb
@@ -6,7 +6,7 @@ class User < ActiveRecord::Base
 
   has_many :user_groups
 
-  POST_LIST = ['ADMIN','COW','AIRY','MONI(OCD)','SG','SAS','WI','CBG','SOC','MOB','INB','CID','247','RND','INFR','SGW','XS','その他']
+  POST_LIST = ['ADMIN','COW','AIRY','MONI(OCD)','SG','SAS','WI','CBG','SOC','MOBA','INB','CID','247','RND','INFR','SGW','XS','その他']
 
   def self.post_options
     POST_LIST.each_with_index.map {|i, post| [ i, post ] }
